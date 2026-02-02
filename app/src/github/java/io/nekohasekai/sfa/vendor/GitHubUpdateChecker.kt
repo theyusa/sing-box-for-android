5ee4c38fad1c51bd274f05b9d5b484cedef1f8cc
theyusa <108272816+theyusa@users.noreply.github.com>
2026-02-02 19:29:00 +0300

    Update GitHubUpdateChecker.kt

--- app/src/github/java/io/nekohasekai/sfa/vendor/GitHubUpdateChecker.kt
+++ app/src/github/java/io/nekohasekai/sfa/vendor/GitHubUpdateChecker.kt
@@ -15,7 +15,7 @@ import java.io.Closeable
 
 class GitHubUpdateChecker : Closeable {
     companion object {
-        private const val RELEASES_URL = "https://api.github.com/repos/sjnhnp/sing-box/releases"
+        private const val RELEASES_URL = "https://api.github.com/repos/TheYusa/sing-box/releases"
         private const val METADATA_FILENAME = "SFA-version-metadata.json"
     }
 
